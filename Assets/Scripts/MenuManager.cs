56f189f7197e5fd961e9ff0fb3d38e29a4313b82
Viacheslav Melnyk <altt1m.zaskar@gmail.com>
2024-05-06 08:19:16 +0300

    Esc bug fixed

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,17 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    private static bool isLoading;
     // Start is called before the first frame update
     void Start()
     {
-
+        isLoading = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().name == "Escape From Homyak")
+        if (Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().name == "Escape From Homyak" && !isLoading)
         {
+            isLoading = true;
             SceneTransition.SwitchToScene("Main Menu");
         }
     }
